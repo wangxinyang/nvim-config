0934f59c7b92373936c15bf04333651c27b76986
Tosei <359387050@qq.com>
2023-06-04 22:35:16 +0900

    fix: delete session to github

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -33,6 +33,29 @@ return {
 		end,
 		opts = {
 			filesystem = {
+				filtered_items = {
+            visible = true, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = true,
+            hide_gitignored = false,
+            hide_hidden = false, -- only works on Windows for hidden files/directories
+            hide_by_name = {
+              --"node_modules"
+            },
+            hide_by_pattern = { -- uses glob style patterns
+              --"*.meta",
+              --"*/src/*/tsconfig.json",
+            },
+            always_show = { -- remains visible even if other settings would normally hide it
+              --".gitignored",
+            },
+            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+              --".DS_Store",
+              --"thumbs.db"
+            },
+            never_show_by_pattern = { -- uses glob style patterns
+              --".null-ls_*",
+            },
+        },
 				bind_to_cwd = false,
 				follow_current_file = true,
 				use_libuv_file_watcher = true,
