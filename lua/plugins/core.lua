68812be4ba60d247247aa2738bc73f7c9bb4fab2
Tosei <359387050@qq.com>
2023-07-06 09:39:36 +0900

    add codeium plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -107,4 +107,15 @@ return {
     },
   	}
   },
+	{
+		event = "VeryLazy",
+		"Exafunction/codeium.vim",
+		config = function ()
+    	-- Change '<C-g>' here to any keycode you like.
+    	vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+    	vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+    	vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+    	vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+  	end
+  },
 }
