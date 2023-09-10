dd3ba0969334d3de7e99c531af211b3d66da2a88
Tosei <359387050@qq.com>
2023-09-10 13:44:49 +0900

    modify cursor highlight problem

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -14,42 +14,6 @@ require("mason").setup({
 	},
 })
 
-local handlers = {
-	-- The first entry (without a key) will be the default handler
-	-- and will be called for each installed server that doesn't have
-	-- a dedicated handler.
-	function (server_name) -- default handler (optional)
-		local highlight_variable = function(client, bufnr)
-			if client.server_capabilities.documentHighlightProvider then
-			-- if client.supports_method "textDocument/documentHighlight" then
-				local lsp_document_highlight = vim.api.nvim_create_augroup("lsp_document_highlight", {})
-				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-					group = lsp_document_highlight,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.document_highlight()
-					end,
-				})
-				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-					group = lsp_document_highlight,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.clear_references()
-					end,
-				})
-			end
-		end
-
-		local opts = {
-			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-			on_attach = function(client, bufnr)
-				highlight_variable(client, bufnr)
-			end,
-		}
-				require("lspconfig")[server_name].setup(opts)
-	end
-}
-
 require("mason-lspconfig").setup({
 	ensure_installed = {
 		"lua_ls",
@@ -66,39 +30,31 @@ require("mason-lspconfig").setup_handlers({
 	-- and will be called for each installed server that doesn't have
 	-- a dedicated handler.
 	function (server_name) -- default handler (optional)
-		local opts = {}
+	--[[ 	local opts = {}
 		opts.on_attach = function(client, bufnr)
 			if client.server_capabilities.documentHighlightProvider then
-				-- if client.supports_method "textDocument/documentHighlight" then
-				vim.cmd [[
-			hi! LspReferenceRead cterm=bold ctermbg=235 guibg=LightYellow
-			hi! LspReferenceText cterm=bold ctermbg=235 guibg=LightYellow
-			hi! LspReferenceWrite cterm=bold ctermbg=235 guibg=LightYellow
-			]]
-				local lsp_document_highlight = vim.api.nvim_create_augroup("lsp_document_highlight", {})
-				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-					group = lsp_document_highlight,
+				vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+				vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+				vim.api.nvim_create_autocmd("CursorHold", {
+					callback = vim.lsp.buf.document_highlight,
 					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.document_highlight()
-					end,
+					group = "lsp_document_highlight",
+					desc = "Document Highlight",
 				})
-				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-					group = lsp_document_highlight,
+				vim.api.nvim_create_autocmd("CursorMoved", {
+					callback = vim.lsp.buf.clear_references,
 					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.clear_references()
-					end,
+					group = "lsp_document_highlight",
+					desc = "Clear All the References",
 				})
 			end
-		end
-
-	--[[ 	local opts = {
+		end ]]
+		--[[ 	local opts = {
 			on_attach = function(client, bufnr)
 				highlight_variable(client, bufnr)
 			end,
 		} ]]
-		require("lspconfig")[server_name].setup(opts)
+		require("lspconfig")[server_name].setup{}
 	end,
 	-- Next, you can provide targeted overrides for specific servers.
 })
@@ -138,6 +94,28 @@ M.on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<C-space>", require("rust-tools").hover_actions.hover_actions, { buffer = bufnr })
 	-- Code action groups
 	vim.keymap.set("n", "<Leader>a", require("rust-tools").code_action_group.code_action_group, { buffer = bufnr })
+	if client.server_capabilities.documentHighlightProvider then
+		vim.cmd [[
+		  set updatetime=400
+			hi! LspReferenceRead cterm=bold ctermfg=159 ctermbg=236 guibg=#FFAF5F
+			hi! LspReferenceText cterm=bold ctermfg=159 ctermbg=236 guibg=#FFAF5F
+			hi! LspReferenceWrite cterm=bold ctermfg=159 ctermbg=236 guibg=#FFAF5F
+			]]
+		vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+		vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+		vim.api.nvim_create_autocmd("CursorHold", {
+			callback = vim.lsp.buf.document_highlight,
+			buffer = bufnr,
+			group = "lsp_document_highlight",
+			desc = "Document Highlight",
+		})
+		vim.api.nvim_create_autocmd("CursorMoved", {
+			callback = vim.lsp.buf.clear_references,
+			buffer = bufnr,
+			group = "lsp_document_highlight",
+			desc = "Clear All the References",
+		})
+	end
 end
 
 -- M.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
@@ -162,7 +140,7 @@ M.capabilities.textDocument.completion.completionItem = {
 
 require("lspconfig").lua_ls.setup(
 	{
-		capabilities = capabilities,
+		capabilities = M.capabilities,
 		settings = {
 			Lua = {
 				runtime = {
